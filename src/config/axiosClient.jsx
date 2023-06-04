7c96f83a4fbdd71702f055b1cf569d4a23a68b1a
rocnail23 <jdanielrojas16@gmail.com>
2023-06-03 20:30:24 -0400

    last commit

--- src/config/axiosClient.jsx
+++ src/config/axiosClient.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const axiosClient = axios.create({
-baseURL: import.meta.env.VITE_API_URL
+baseURL: "https://usercrud-4oii.onrender.com/"
 })
 
 export default axiosClient
